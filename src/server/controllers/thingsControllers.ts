cf136c12cdcb7589ae6e75bffa03edda3847f75d
Raúl Pérez <jrpcassone@gmail.com>
2023-08-21 23:04:12 +0200

    Add function to delete a thing from the API rest by id

--- src/server/controllers/thingsControllers.ts
+++ src/server/controllers/thingsControllers.ts
@@ -2,6 +2,8 @@ import { type Request, type Response } from "express";
 import knownThings from "../data/things.js";
 
 export const getThings = (_req: Request, res: Response) => {
+  console.log("Things got delivered by GET method.");
+
   res.status(200).json({ knownThings });
 };
 
@@ -10,5 +12,19 @@ export const getThingById = (req: Request, res: Response) => {
 
   const foundThing = knownThings.find((thing) => thing.id === Number(idThing));
 
+  console.log("Thing got delivered by GET method.");
+
   res.status(200).json(foundThing);
+};
+
+export const deleteThingById = (req: Request, res: Response) => {
+  const { idThing } = req.params;
+
+  const thingToDeletePosition = knownThings.findIndex(
+    (thing) => thing.id === Number(idThing)
+  );
+
+  knownThings.splice(thingToDeletePosition, 1);
+
+  res.status(200).json({ message: `Thing nº${idThing} got deleted` });
 };
--- src/server/routes/thingsRoutes.ts
+++ src/server/routes/thingsRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getThingById, getThings } from "../controllers/thingsControllers.js";
+import {
+  deleteThingById,
+  getThingById,
+  getThings,
+} from "../controllers/thingsControllers.js";
 
 export const thingsRoutes = express.Router();
 
 thingsRoutes.get("/", getThings);
 thingsRoutes.get("/:idThing", getThingById);
+thingsRoutes.delete("/:idThing", deleteThingById);
 
 export default thingsRoutes;
