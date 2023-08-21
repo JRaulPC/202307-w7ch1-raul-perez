f685bf66db52cb3bed185b98af9a52dd38d0d33f
Raúl Pérez <jrpcassone@gmail.com>
2023-08-22 00:42:49 +0200

    Add function to create a new thing in API rest by POST method

--- src/server/controllers/thingsControllers.ts
+++ src/server/controllers/thingsControllers.ts
@@ -1,5 +1,7 @@
 import { type Request, type Response } from "express";
 import knownThings from "../data/things.js";
+import { type Thing } from "../data/types.js";
+import { type CustomRequest } from "./types.js";
 
 export const getThings = (_req: Request, res: Response) => {
   console.log("Things got delivered by GET method.");
@@ -27,4 +29,17 @@ export const deleteThingById = (req: Request, res: Response) => {
   knownThings.splice(thingToDeletePosition, 1);
 
   res.status(200).json({ message: `Thing nº${idThing} got deleted` });
+};
+
+export const createThingByBody = (req: CustomRequest<Thing>, res: Response) => {
+  const newThing: Thing = req.body;
+
+  if (!newThing) {
+    return new Error("value must be defined");
+  }
+
+  const newThings = [...knownThings, newThing];
+
+  console.log(req.body);
+  res.status(201).json(newThings);
 };
--- src/server/routes/thingsRoutes.ts
+++ src/server/routes/thingsRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  createThingByBody,
   deleteThingById,
   getThingById,
   getThings,
@@ -10,5 +11,7 @@ export const thingsRoutes = express.Router();
 thingsRoutes.get("/", getThings);
 thingsRoutes.get("/:idThing", getThingById);
 thingsRoutes.delete("/:idThing", deleteThingById);
+
+thingsRoutes.post("/", createThingByBody);
 
 export default thingsRoutes;
