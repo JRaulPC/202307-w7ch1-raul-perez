e58e43f2c48119925a650215ae9fe511b4580d8f
Raúl Pérez <jrpcassone@gmail.com>
2023-08-21 20:41:07 +0200

    Add response types and fix object

--- src/server/data/things.ts
+++ src/server/data/things.ts
@@ -1,9 +1,9 @@
-const knownThings = {
-  things: [
-    { thing: "Anger management" },
-    { thing: "React" },
-    { thing: "No seguir copiando" },
-  ],
-};
+import { type Thing } from "./types";
+
+const knownThings: Thing[] = [
+  { thing: "Anger management" },
+  { thing: "React" },
+  { thing: "No seguir copiando" },
+];
 
 export default knownThings;
